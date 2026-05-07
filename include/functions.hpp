86a9fee55aa53656efef2a94a08e9e15b119caa6
Gui1-p <guilhermepaivaperes@gmail.com>
2026-05-07 15:37:53 -0300

    Add batalha_1v1 function declaration

--- include/functions.hpp
+++ include/functions.hpp
@@ -10,6 +10,7 @@
 void ler_nomes(string nomeArquivo, vector<string> &vetorNomes, size_t tamanhoExercito);
 void criar_exercito(vector<Player> &raca, const vector<string> &vetorNomes, bool humano);
 void imprimir_exercito(vector<Player> &raca, string nomeRaca);
+bool batalha_1v1(Player &individuo, Player &oponente, vector<Player> &time_oponente, string FirstRace, string SecondRace, size_t indice);
 string batalhar(vector<Player> &Humanos, vector<Player> &Orcs);
 void a_ultima_esperanca(vector<Player> &Orcs);
 void milagre_real(vector<Player> &Humanos);
